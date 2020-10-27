822e97e5b53135d0d6dc6ddb32358bace756f96d
Joey Kwestro <99062903@mydavinci.nl>
2020-10-27 14:15:26 +0100

    3 af, begonnen met 4

---
+++ lab 1/opdracht 3/opdracht3.js
@@ -0,0 +1,8 @@
+for(var i = "stop"; i != naam;){
+var naam = prompt("Wat is de naam?");
+var leeftijd = prompt("Wat is de leeftijd?");
+document.write(naam);
+document.write(" ");
+document.write(leeftijd);
+document.write("<br>");
+}
---
+++ lab 1/opdracht 4/opdracht4.js
@@ -0,0 +1,5 @@
+var num = prompt("Hoeveel tafels wilt u uitrekenen?");
+
+if(num = 1){
+document.write()
+}
