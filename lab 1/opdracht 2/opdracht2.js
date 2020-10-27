43983b8514eeaa105bdc6199e24cc8662c50b479
Joey Kwestro <99062903@mydavinci.nl>
2020-10-27 12:51:18 +0100

    2 afgemaakt

--- lab 1/opdracht 2/opdracht2.js
+++ lab 1/opdracht 2/opdracht2.js
@@ -1,16 +1,65 @@
-function incresement() {
-	var num3 = 0
-	var nummer = 0
-	var button1 = document.createElement("button");
-	button1.innerHTML = "+1";
-
-	var body = document.getElementsByTagName("body")[0];
-	body.appendChild(button1);
-
-	button1.addEventListener ("click", function() {
-		document.write(nummer)
-	});
-	document.write(num3 + nummer);
+function addition(){
+	var ans = (10 + 12);
+	document.write("10 + 12 = ");
+	document.write(ans);
 }
 
+
+
+function subtraction(){
+	var ans = (58 - 34);
+	document.write("58 - 34 = ");
+	document.write(ans);
+}
+
+
+
+function multiplication(){
+	var ans = (6 * 7);
+	document.write("6 x 7 = ");
+	document.write(ans);
+}
+
+
+function division(){
+	var ans = (144 / 12);
+	document.write("144 / 12 = ");
+	document.write(ans);
+}
+
+
+function incresement(){
+	var num = prompt("hoeveel keer wilt u 1 bij 0 optellen?");
+	document.write("0");
+	for(var i = 1; i <= num; i++){
+		document.write(" + 1");
+		document.write();
+	}
+	document.write(" = ");
+	document.write(num);
+}
+
+
+function decresement(){
+	var num = prompt("hoeveel keer wilt u 1 bij 1000 aftrekken?");
+	document.write("1000");
+	for(var i = 1; i <= num; i++){
+		document.write(" - 1");
+		document.write();
+	}
+	document.write(" = ");
+	var ans = (1000 - num);
+	document.write(ans);
+}
+
+addition();
+document.write("<br>");
+subtraction();
+document.write("<br>");
+multiplication();
+document.write("<br>");
+division();
+document.write("<br>");
 incresement();
+document.write("<br>");
+decresement();
